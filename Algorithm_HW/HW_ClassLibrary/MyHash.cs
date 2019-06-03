2f44a36f90731096e0f9d6d53c4be11d0f8b2a00
gad <gad@gad>
2019-06-03 12:21:15 +0300

    HW6

---
+++ Algorithm_HW/HW_ClassLibrary/MyHash.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+
+namespace HW_ClassLibrary
+{
+    public static class MyHash
+    {
+        public static int Hash(string x)
+        {
+            int p = 42; //Просто число
+            int rez = 0; //Результат вычисления 
+            for (int i = 0; i < x.Length; i++)
+            {
+                rez += (int)Math.Pow(p, x.Length - 1 - i) * (int)(x[i]);//Подсчет хеш-функции
+            }
+            return rez;
+        }
+    }
+}
